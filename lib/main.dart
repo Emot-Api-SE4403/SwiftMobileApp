67cc096d00d3d1ead3e8aaf6eea029ee33c217e3
Aditya MP <211wadityaw112@gmail.com>
2023-05-15 16:06:28 +0700

    main

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,7 @@ import 'home.dart';
 import 'Dashboard.dart';
 import 'profil.dart';
 
-void main() async {
-  runApp(MyApp());
-}
+void main() => runApp(new MyApp());
 
 AndroidOptions _getAndroidOptions() => const AndroidOptions(
       encryptedSharedPreferences: true,
